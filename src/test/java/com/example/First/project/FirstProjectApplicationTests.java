b61f2edf5a1e912f176789f9196d65a5b9cc46ee
minzzun99 <skg06123@naver.com>
2023-12-11 19:20:46 +0900

    12월11일 commit test

---
+++ src/test/java/com/example/First/project/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.First.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
