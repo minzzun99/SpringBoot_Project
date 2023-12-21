652154ca30c694dbc56e46e23f22252705cfa394
minzzun99 <skg06123@naver.com>
2023-12-21 19:33:38 +0900

    12월21일 7장 나머지 실습

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (id,title,content) VALUES (1, '강민준','aaaa');
+INSERT INTO article (id,title,content) VALUES (2, '김상학','bbbb');
+INSERT INTO article (id,title,content) VALUES (3, '박지수','cccc');
