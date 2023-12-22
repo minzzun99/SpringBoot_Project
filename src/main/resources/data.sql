4818ccea8685e01c816fe08cea5322a71425d492
minzzun99 <skg06123@naver.com>
2023-12-22 18:09:12 +0900

    12월22일 9장 실습

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article (id,title,content) VALUES (1, '강민준','aaaa');
-INSERT INTO article (id,title,content) VALUES (2, '김상학','bbbb');
-INSERT INTO article (id,title,content) VALUES (3, '박지수','cccc');
+INSERT INTO article (title,content) VALUES ('강민준','aaaa');
+INSERT INTO article (title,content) VALUES ('김상학','bbbb');
+INSERT INTO article (title,content) VALUES ('박지수','cccc');
